e0c5b1ca07e73747f4f751e0a56d9324b12d98a9
podluzhnyyK <podluzhnyy94@mail.ru>
2022-09-01 17:42:30 +0500

    fix num2

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,11 @@
+import readlineSync from 'readline-sync';
+
 export default function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function getName() {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
 }
